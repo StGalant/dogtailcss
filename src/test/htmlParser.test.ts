9bd6a923cd95c8d622377c89358883cb55e42508
Vladimir Y <st.galant@gmail.com>
2021-08-06 11:44:49 +0000

    add a simple HTML parser

---
+++ src/test/htmlParser.test.ts
@@ -0,0 +1,17 @@
+import { parseHtml } from '../file-utils/parseHtmlClasses'
+
+let simpleHtml = `<div class="class-1
+lg:hover:class-2
+class-3-(1,2,3)
+" attr="data"><span class="2xl:class-4-#span"> class="herabora" </span></div>`
+
+test('get list of classes from simple html', () => {
+  let expected = new Set<string>([
+    'class-1',
+    'lg:hover:class-2',
+    'class-3-(1,2,3)',
+    '2xl:class-4-#span',
+  ])
+  let result = parseHtml(simpleHtml)
+  expect(result).toEqual(expected)
+})
